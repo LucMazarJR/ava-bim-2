dcfaa7c7eb15e85e235b2ee41fdfd524473149cd
LucianoMazar <lmazaraojr@gmail.com>
2026-05-12 10:33:23 -0300

    feat(user): add mongose and user

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule.forRoot(process.env.MONGODB_URI ?? '')],
 })
 export class AppModule {}
