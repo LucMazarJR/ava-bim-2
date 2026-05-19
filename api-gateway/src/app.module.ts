6935d817c53480be4ac282ddf2f5e9152bb309db
LucianoMazar <lmazaraojr@gmail.com>
2026-05-19 20:04:20 -0300

    chore(gateway): change guards directory name

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/src.guard';
 
 @Module({
   imports: [
@@ -12,6 +14,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
