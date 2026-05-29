be2b22f0e4dac75d9b118ecfcb89de9a99cf3ab0
LucianoMazar <lmazaraojr@gmail.com>
2026-05-29 11:20:21 -0300

    refactor(core): standardize env dependencies

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JwtModule } from '@nestjs/jwt';
@@ -10,6 +11,7 @@ import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     HttpModule,
     JwtModule.register({
       global: true,
--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot(process.env.MONGODB_URI ?? '')],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI ?? ''),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
